1f15c02e4907dd6d2a203bcf9c6f3f40d0140525
Wei Shen Tan <tanweishen97@gmail.com>
2023-04-09 13:16:40 +0800

    Added Camera animate on mouse move logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { Vector3 } from 'three'
 
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
@@ -8,8 +9,11 @@ function Box(props) {
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
+
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+  });
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
@@ -25,14 +29,45 @@ function Box(props) {
   )
 }
 
+function AnimateCamera() {
+  const mouse = {
+    x: 0,
+    y: 0,
+  }
+  useEffect(() => {
+    window.m = mouse;
+    const mouseMoveListener = (e) => {
+      const { innerWidth, innerHeight } = window;
+      const { clientX, clientY } = e;
+      /* Normalize the values to be [-0.5..0.5] */
+      const mouseX = (clientX / innerWidth) - 1;
+      const mouseY = ((clientY / innerHeight) - 1) * -1;
+      mouse.x = mouseX;
+      mouse.y = mouseY;
+    }
+    window.addEventListener('mousemove', mouseMoveListener);
+
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveListener);
+    }
+  }, []);
+
+  useFrame((state) => {
+    const lookAt = new Vector3(0, 0, -1);
+    lookAt.applyQuaternion(state.camera.quaternion);
+    lookAt.add(new Vector3(mouse.x, mouse.y, 0));
+    state.camera.lookAt(lookAt);
+  });
+}
+
 export default function App() {
   return (
     <Canvas>
+      <AnimateCamera/>
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box position={[0, 0, 0]} />
       <OrbitControls />
     </Canvas>
   )
